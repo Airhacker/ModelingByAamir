ac81936406ed87cb9639944bb32c0cd78298faa1
Airhacker <umairahmed.dev@gmail.com>
2020-04-05 19:06:42 -0500

    All pages and some animations

--- javascript/app.js
+++ javascript/app.js
@@ -16,3 +16,70 @@ links.forEach(link => {
   });
 });
 */
+
+// Work Page
+
+const workBtn = document.querySelector(".work-btn");
+const closeBtn = document.querySelector("#close-btn");
+const body = document.querySelector("body");
+
+let tl = gsap.timeline();
+
+// Work Button Settings
+workBtn.addEventListener("click", () => {
+  // Background Animation
+  tl.to("#work-page", {
+    duration: 1,
+    opacity: 1,
+    y: -window.innerHeight,
+  })
+    // Page Heading Animation
+    .fromTo(
+      ".work-heading",
+      {
+        opacity: 0,
+        y: 100,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      }
+    )
+    // Image Gallery Animation
+    .from(".image-gallery img", {
+      y: 200,
+      opacity: 0,
+      delay: 0.25,
+      stagger: {
+        amount: 1,
+        ease: "ease",
+      },
+    });
+
+  // Overflow Settings
+  body.style.overflowY = "scroll";
+});
+
+// Close Button Settings
+closeBtn.addEventListener("click", () => {
+  gsap.to("#work-page", {
+    duration: 2,
+    opacity: 0,
+    y: window.innerHeight,
+  });
+
+  body.style.overflow = "hidden";
+});
+
+// Contact Page
+
+const contactBtn = document.querySelector(".contact-btn");
+
+// Contact Button
+contactBtn.addEventListener("click", () => {
+  tl.to("#contact-page", {
+    duration: 1,
+    opacity: 1,
+    x: -window.innerWidth,
+  }).fromTo("");
+});
